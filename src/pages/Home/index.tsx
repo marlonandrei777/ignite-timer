4adcbe4bf040e7446654a5eebac273c69c7b4566
Marlon Andrei <marlon-master@hotmail.com>
2023-05-26 15:19:40 -0300

    go

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -11,6 +12,7 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { differenceInSeconds } from 'date-fns'
 
 /* schema de validacao. validar os dados do form baseado no formato a baixo */
 const newCyrcleFormValidateionSchema = zod.object({
@@ -20,14 +22,22 @@ const newCyrcleFormValidateionSchema = zod.object({
   minutesAmount: zod.number().min(5).max(60),
 })
 
-type NewCycleFromData = {
+/* tipagem dos inputs do form tirados de dentro do schema do zod */
+type NewCycleFromData = zod.infer<typeof newCyrcleFormValidateionSchema>
+interface Cycle {
+  id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
   /* passamos para o useFrom o objeto de configuracoes */
-  const { register, handleSubmit, watch } = useForm<NewCycleFromData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFromData>({
     /* passamos para dentro de zod zodResolver, qual é o schema de validacao,
     ou seja, de q forma queremos validar os dados q temos nos inputs,
     as regras de validação */
@@ -38,10 +48,64 @@ export function Home() {
     },
   })
 
+  /* tentar com find */
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  // intervalor
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+
+    /* quando executar o useeffect de novo, vamos fazer algo
+    para limpar/resetar o useEfect anterior */
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle])
+
   /* data: dados dos nossos inputs do formulario */
-  function handleCreateNewCicle(data: any) {
-    console.log(data)
+  function handleCreateNewCicle(data: NewCycleFromData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    /* padrao de add estado
+    Sempre q uma alteracao de estado depender do valor anterior
+    usamos o formato de arrow function; */
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
+    reset()
   }
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  // atualizando o titulo da aba da pagina
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   /* observando o campo de task */
   const task = watch('task')
@@ -84,11 +148,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
