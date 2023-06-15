3cbced6b8d39db9d5c2e71d88e3460f81d046d66
Marlon Andrei <marlon-master@hotmail.com>
2023-06-15 14:00:47 -0300

    Reducers

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useReducer, useState } from 'react'
 
 interface CreateCycleData {
   task: string
@@ -33,14 +33,59 @@ type CylclesContextProviderProps = {
   children: ReactNode
 }
 
+type CyclesStage = {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 // crianco o componente principal
 export function CyclesContextProvider({
   children,
 }: CylclesContextProviderProps) {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  // agr com o dispatch, vamos disparar a ação
+  const [cyclesState, dispatch] = useReducer(
+    (state: CyclesStage, action: any) => {
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          }
+        case 'INTERRUPT_CURRENT_CYCLE':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, interruptedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleId: null,
+          }
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleId: null,
+          }
+        default:
+          return state
+      }
+    },
+    { cycles: [], activeCycleId: null },
+  )
+
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
+  const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   function setSecondsPassed(seconds: number) {
@@ -48,15 +93,12 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsfinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
+    dispatch({
+      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      payload: {
+        data: activeCycleId,
+      },
+    })
   }
 
   /* data: dados dos nossos inputs do formulario */
@@ -70,27 +112,31 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
+    /* dentro do dispatch enviamos alguma info q lá dentro do reducer
+    consegamos distinguir uama action da outra, ou seja, enviamos um objeto
+    enviamos um type e dentro desse type dizemos qual ação queremos realizar */
+    dispatch({
+      type: 'ADD_NEW_CYCLE',
+      payload: {
+        newCycle,
+      },
+    })
+
     /* padrao de add estado
     Sempre q uma alteracao de estado depender do valor anterior
     usamos o formato de arrow function; */
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
+    /* setCycles((state) => [...state, newCycle]) */
     setAmountSecondsPassed(0)
   }
 
   // parando o ciclo e colocando a hora do ciclo pausado na variavel interruptedDate
   function interruptCurrentCycle() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
-
-    setActiveCycleId(null)
+    dispatch({
+      type: 'INTERRUPT_CURRENT_CYCLE',
+      payload: {
+        activeCycleId,
+      },
+    })
   }
 
   return (
